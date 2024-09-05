b91f8318979eddd2703f1b11686319cf923be842
Bryson <brysob7617@gmail.com>
2024-09-04 23:16:19 -0400

    added models for user and thoughts

---
+++ models/Thought.js
@@ -0,0 +1,43 @@
+const { Schema, model, now } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: currentTime => new Date(currentTime).toLocaleString(),
+
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: {
+
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
