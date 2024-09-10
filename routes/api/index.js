9d23f8077b5e810c8d4992e3589bdca09b673377
Bryson <brysob7617@gmail.com>
2024-09-09 23:04:59 -0400

    renamed database name

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
-const reactionRoutes = require('./reaction-routes');
 
-router.use('./user', userRoutes);
-router.use('./thought', thoughtRoutes);
-router.use('./reaction', reactionRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
